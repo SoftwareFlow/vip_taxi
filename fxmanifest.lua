93dae69c4046d1c8b6e1ed236799076de74d83d4
SoftwareFlow <koessling-florian@t-online.de>
2025-02-05 00:38:07 +0100

    Created fxmanifest.lua for vip_taxi
    essential for starting/ensure a ressource in FiveM

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'SoftwareFlow'
+description 'NPC Taxi for FiveM'
+version '1.0.0'
+
+client_scripts {
+    'client/master.lua'
+}
+
+server_scripts {
+    -- for later use, placeholder
+}
